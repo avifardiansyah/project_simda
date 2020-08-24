8450fdf01b77f767d55916f89598cb7386af1ff5
avifardiansyah <gapunyasaya@gmail.com>
2020-08-24 12:09:46 +0700

    inisiasi module_spj

---
+++ app/Http/Controllers/module_spj/spj_dinas/Dashboard_dinas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\module_spj\spj_dinas;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Dashboard_dinas extends Controller
+{
+    return view('module_spj/vDinas/_dashboard_dinas');
+}
